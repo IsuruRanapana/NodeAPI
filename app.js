d4942b80ccce6d5094f48ba81f5259443fdb1006
IsuruRanapana <113498635+IsuruRanapana@users.noreply.github.com>
2022-11-15 15:30:11 +0530

    added routes and middleware

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 const port = 8000;
 
-app.get('/', (req, res) => {
-    res.send('Hello world from NodeJS')
-});
+///bringing routes
+const {getPosts} = require("./routes/post");
+
+//middleware
+app.use(morgan('dev'));
+
+app.get('/', getPosts);
 
 app.listen(port, '127.0.0.1', () => {
     console.log(`A node js API is listening on port ${port}`);
